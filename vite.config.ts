1c22134beec1eeb7eed37633917bc35c0664c8a6
Paradox <paradox.akshay@gmail.com>
2023-06-11 14:05:49 +0530

    setup git pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        assetFileNames: () => {
-          return `apitester-creator/[name]-[hash][extname]`;
-        },
-        chunkFileNames: "apitester-creator/[name]-[hash].js",
-        entryFileNames: "apitester-creator/[name]-[hash].js",
-      },
-    },
-  },
+  base: "./",
 });
